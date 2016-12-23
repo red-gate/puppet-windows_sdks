593c4f3973bf693f1f7065ba06e07e8256c0d116
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-12-23 11:51:38 +0000

    add missing spec_windowshelper.rb

--- spec/acceptance/dotnet462_spec.rb
+++ spec/acceptance/dotnet462_spec.rb
@@ -1,5 +1,5 @@
 # Encoding: utf-8
-require_relative '../../../spec_windowshelper'
+require_relative 'spec_windowshelper'
 
 describe package('Microsoft .NET Framework 4.6.2 SDK') do
   it { should be_installed }
