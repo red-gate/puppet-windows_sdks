9b7b39e46caa3bbb57042b67ac512180e17896f3
Robert Chipperfield <robert.chipperfield@red-gate.com>
2018-04-03 15:29:31 +0100

    Add .NET 4.7.1 SDK

---
+++ manifests/dotnet471.pp
@@ -0,0 +1,22 @@
+# Install the .NET Framework 4.7 DevPack
+class windows_sdks::dotnet47(
+  $source = 'https://download.microsoft.com/download/9/0/1/901B684B-659E-4CBD-BEC8-B3F06967C2E7/NDP471-DevPack-ENU.exe',
+  $temp_folder = 'C:/Windows/Temp'
+  ) {
+
+  require archive
+
+  $filename = inline_template('<%= File.basename(@source) %>')
+
+  ensure_resource('file', $temp_folder, { ensure => directory })
+
+  archive { "${temp_folder}/${filename}":
+    source  => $source,
+    require => File[$temp_folder],
+  }
+  ->
+  package { 'Microsoft .NET Framework 4.7.1 SDK':
+    source          => "${temp_folder}/${filename}",
+    install_options => ['/quiet', '/norestart'],
+  }
+}
