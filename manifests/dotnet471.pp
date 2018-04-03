f04e15db167311f0aace9791caf28c036bfb6bfb
Robert Chipperfield <robert.chipperfield@red-gate.com>
2018-04-03 15:41:16 +0100

    Remember to update class names when you duplicate them...

--- manifests/dotnet471.pp
+++ manifests/dotnet471.pp
@@ -1,5 +1,5 @@
 # Install the .NET Framework 4.7 DevPack
-class windows_sdks::dotnet47(
+class windows_sdks::dotnet471(
   $source = 'https://download.microsoft.com/download/9/0/1/901B684B-659E-4CBD-BEC8-B3F06967C2E7/NDP471-DevPack-ENU.exe',
   $temp_folder = 'C:/Windows/Temp'
   ) {
